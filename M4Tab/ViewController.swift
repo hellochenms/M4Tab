258f56d52d181f81d97b6d2d4d31858ba06def8f
chenmeisong <chenmeisong@baidu.com>
2019-05-23 15:29:18 +0800

    init

---
+++ M4Tab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  M4Tab
+//
+//  Created by Chen,Meisong on 2019/5/23.
+//  Copyright © 2019 xyz.chenms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
